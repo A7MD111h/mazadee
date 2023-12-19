3fe3bb5bae237b2263466c4db78591d9db70b8e9
omarsalah0804 <omarsalah0804@gmail.com>
2023-12-19 20:26:01 +0300

    omar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MAZADe</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="shortcut icon" href="/img/icon-logo.png" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/5f8f97e3fd.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <section id="loader">
+        <div class="img-loader">
+        </div>
+    </section>
+
+    <header>
+        <div class="header-top">
+            <div class="container">
+                <div class="content d-flex justify-content-between align-items-center">
+                    <ul class="d-flex">
+                        <li>
+                            <a href="tel:+962000000000" class="mr-3">
+                                <i class="fas fa-phone-alt"></i>
+                                <span>+962000000000</span>
+                            </a>
+                        </li>
+                        <li>
+                            <i class="fas fa-globe"></i>
+                            <select name="language" class="select-bar">
+                                <option value="en">En</option>
+                                <option value="az">Ar</option>
+                            </select>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- Navbar -->
+        <div class="header-bottom">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container">
+                    <div class="logo">
+                        <a class="navbar-brand" href="/">
+                            <img src="/img/logo.png" alt="logo">
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <i class="fas fa-bars navbar-toggler-icon"></i>
+                        </button>
+                    </div>
+
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Auction</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="">About Us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Contact</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/sing-in">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="sing-up">Sing Up</a>
+                            </li>
+                            <li>
+                                <a href="" class="nav-link" style="background-color: #ee4730; border-radius: 50%;">
+                                <i class="fas fa-user-alt" style="color: #fff ;line-height: inherit;"></i>
+                            </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <!-- End Navbar -->
+    </header>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.countdown/2.2.0/jquery.countdown.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="/js/main.js"></script>
+</body>
+
+</html>
